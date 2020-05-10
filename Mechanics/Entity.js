66df4d454ca11fe9971bb057dbc41a37e0f1bd3f
megaliteroDelfini <57036626+odimiosdimos@users.noreply.github.com>
2020-05-10 18:46:39 +0300

    mechanics start

---
+++ Mechanics/Entity.js
@@ -0,0 +1,24 @@
+class Entity {
+    constructor(w=0,h=0,posX=0,posY=0) {
+        this.pos = new Vec2(posX, posY);
+        this.vel = new Vec2(0, 0);
+        this.size = new Vec2(w,h)
+
+        this.traits = []
+    }
+
+    addTrait(trait){
+        this.traits.push(trait)
+
+        //creates a new variable (like in the constructor)
+        this[trait.NAME] = trait;
+    }
+
+
+    update(deltaTime) {
+        this.traits.forEach( trait =>{
+            trait.update(this,deltaTime)
+        } )
+
+    }
+}
---
+++ Mechanics/Traits.js
@@ -0,0 +1,75 @@
+class Trait {
+    constructor(name){
+        this.NAME = name
+
+    }
+
+    update(){
+        console.warn('Unhandled update call');
+    }
+
+}
+
+//the traits
+
+class Velocity extends Trait{
+    constructor(){
+        super('Velocity')
+    }
+
+    update(entity,deltaTime){
+        //max velocity should be less than tuileSize per frame
+        if (entity.vel.y > 500) entity.vel.y = 500;
+
+        entity.pos.x += entity.vel.x*deltaTime;
+        entity.pos.y += entity.vel.y*deltaTime;
+    }
+
+}
+
+class Gravity extends Trait {
+    constructor(){
+        super('Gravity')
+        this.G=0;//this.G = 500;
+    }
+
+    update(entity,deltaTime){
+        entity.vel.y+=this.G**entity.M*deltaTime
+    }
+
+}
+
+class Jump extends Trait {
+    constructor(){
+        super('Jump')
+
+        //h taxithta tou phdhmatos
+        this.velocity = 200;
+
+        //h megisth diarkeia tou phdhmatos
+        this.duration = 0.3;
+
+        //o sinolikos xronos pou einai pathmeno to Jump
+        this.engageTime=0;
+    }
+
+    start(){
+        this.engageTime=this.duration;
+    }
+
+    cancel(){
+        this.engageTime=0;
+    }
+
+    update(entity,deltaTime){
+        if (this.engageTime > 0){
+            entity.vel.y = -this.velocity;
+            this.engageTime -= deltaTime
+        }
+    }
+
+}
+
+class Collider{
+    //not implemented yet
+}
